d7cec1258400673e06d212114265ea633cae493a
Naomi Isbell <naomiisbell@gmail.com>
2020-07-22 10:29:41 -0400

    moved all assets to src folder

---
+++ src/components/Modal.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Item1 from './assets/matches.png'
+
+class Modal extends React.Component {
+    render() {
+        return (
+            <div className="firstImg" >
+                <img className="matchesImg" src={Item1}></img>
+            </div>
+        )
+    }
+}
+
+export default Modal;
