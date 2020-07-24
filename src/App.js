2ba356964ba7f0f5195f2d6b2808c2423661961b
Naomi Isbell <naomiisbell@gmail.com>
2020-07-24 10:33:56 -0400

    deleted components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import Modal from './components/Modal'
 
 function App() {
   return (
     <body>
     <div className="App">
-      <Modal />
+
     </div>
     </body>
   );
