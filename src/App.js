91b10207064034747167c6cf81e8ff70d8660d33
Naomi Isbell <naomiisbell@gmail.com>
2020-07-27 23:21:41 -0400

    added NavBar.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Modal from './components/Modal'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <body>
     <div className="App">
-
+      <Modal />
+      <NavBar />
     </div>
     </body>
   );
